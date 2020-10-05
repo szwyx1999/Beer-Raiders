fdcffbcaed481a6d99e6d4b5342bc6f2a363ee2a
Chris-Davila <69489724+Chris-Davila@users.noreply.github.com>
2020-10-05 11:55:24 -0600

    Addition of images and movement mechanics.. needs updating

---
+++ beerRaiders.py
@@ -0,0 +1,47 @@
+import pygame
+
+# initialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((950, 750))
+
+# Background
+background = pygame.image.load('barOutdoor.png')
+
+# Title and Icon
+pygame.display.set_caption("Beer Raiders")
+icon = pygame.image.load('alcohol.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('Shoot (1).png')
+playerX_change = 0
+playerX = 475
+playerY = 600
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+# Game Loop
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # if keystroke is pressed check if left or right arrow
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
+    pygame.display.update()
